2c601180c558e9347a75c5b2c2501678becf9b27
Alex Moores <moores.alexd@gmail.com>
2017-03-02 01:28:20 -0500

    Added baseline functionality 	Command-line functionality 	does not handle images of different size 	assumes images have same alpha

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cpp-Anaglyph
 
-Given a stereo pair of monochrome PNG images, combine the two images into a single red-cyan anaglyph image and output the result to another PNG file
+Given a stereo pair of monochrome PNG images, combine the two images into a single red-cyan anaglyph image and output the result to another PNG file. use of an open source PNG library like libpng is allowed.
 
 Extra: Use GLSL and OpenGL to do the combination, and output the result to the screen
---
+++ anaglyph.cpp
@@ -0,0 +1,46 @@
+// left: red, right: cyan
+// FF0000     00FFFF
+
+#include <iostream>
+#include "lodepng.h"
+
+int main(int argc, char **argv) {
+
+  if (argc != 4) {
+    std::cout << "Usage: anaglyph <right_image> <left_image> <output_image> \n";
+    return 0;
+  }
+
+  const char* rightFilename = argv[1];
+  const char* leftFilename = argv[2];
+  const char* outputFilename = argv[3];
+
+  std::vector<unsigned char> rightImage;
+  unsigned rightWidth, rightHeight;
+
+  std::vector<unsigned char> leftImage;
+  unsigned leftWidth, leftHeight;
+
+  unsigned rightError = lodepng::decode(rightImage, rightWidth, rightHeight, rightFilename);
+  if(rightError) std::cout << "decoder error " << rightError << ": " << lodepng_error_text(rightError) << std::endl;
+  unsigned leftError = lodepng::decode(leftImage, leftWidth, leftHeight, leftFilename);
+  if(leftError) std::cout << "decoder error " << leftError << ": " << lodepng_error_text(leftError) << std::endl;
+
+  std::vector<unsigned char> newImage;
+  newImage.assign(rightImage.size(), 0);
+
+  for (unsigned i = 0; i < rightImage.size(); i+= 4) {
+    newImage[i] = rightImage[i];
+    newImage[i+1] = leftImage[i+1];
+    newImage[i+2] = leftImage[i+2];
+    newImage[i+3] = leftImage[i+3];
+  }
+
+  unsigned newImageError = lodepng::encode(outputFilename, newImage, rightWidth, rightHeight);
+
+  if(newImageError) std::cout << "encoder error " << newImageError << ": "<< lodepng_error_text(newImageError) << std::endl;
+
+  return 0;
+
+}
+
